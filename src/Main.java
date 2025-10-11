f29cd446aaf274b06bdcf1ff2d6194794a3893ab
Anna kornafel <anna.kornafel01@gmail.com>
2025-10-11 17:43:47 +0200

    Subtraktionsmethode hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,17 @@ public class Main {
         int r2 = Calculator.add(56, 7);
         int r3 = Calculator.add(139, 18);
 
+        int r4 = Calculator.substraction(89, 65);
+        int r5 = Calculator.substraction(5229, 864);
+        int r6 = Calculator.substraction(56, 79);
+
 
         System.out.println(r1);
         System.out.println(r2);
         System.out.println(r3);
+
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r6);
     }
 }
