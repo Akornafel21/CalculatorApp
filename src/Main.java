370b58db69cfcffc82233476a7d0444bbd9880eb
Anna kornafel <anna.kornafel01@gmail.com>
2025-10-11 17:40:27 +0200

    Initial commit: Projekt angelegt und einfache Addition implementiert

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int r1 = Calculator.add(1, 2);
+        int r2 = Calculator.add(56, 7);
+        int r3 = Calculator.add(139, 18);
+
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+    }
+}
