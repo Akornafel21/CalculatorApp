5b97288333afe55e9a0f66dea6773aa275129862
Anna kornafel <anna.kornafel01@gmail.com>
2025-10-26 15:38:02 +0100

    Package erstellt

--- src/uebung1/Calculator.java
+++ src/uebung1/Calculator.java
@@ -1,9 +1,9 @@
+package uebung1;
+
 public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
 
-    public static int substraction(int a, int b) {
-        return a - b;
-    }
+    public static int substraction(int a, int b) { return a - b; }
 }
