5f5c0fa4345bb412a59e2666adaffd3c9662a1c2
Anna kornafel <anna.kornafel01@gmail.com>
2025-11-03 17:28:37 +0100

    IllegalArgumentException in der Klasse Patient realisirt

--- src/uebung2/Patient.java
+++ src/uebung2/Patient.java
@@ -9,15 +9,6 @@ public class Patient implements Displayable {
     // ein Konstruktor initialisiert die Attribute
     // die Werte werden übernommen und oben gespeichert
     public Patient(String name, int patientId) {
-        // Überprüfung, ob der Name gültig ist:
-        // 1. name == null → kein Wert übergeben
-        // 2. name.trim().isEmpty() → der Name besteht nur aus Leerzeichen
-        // trim() entfernt alle Leerzeichen am Anfang und Ende des Strings.
-        // Wenn der Name also z. B. "   " ist, wird er nach trim() zu "",
-        // und isEmpty() gibt true zurück → Exception wird geworfen.
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Ungültiger Name: Name darf nicht leer oder nur aus Leerzeichen bestehen.");
-        }
         this.name = name;
         this.patientId = patientId;
     }
